abc2c223a10959fb9bde75f57b322850f8c21a0f
Quallenauge <Hamsi2k@freenet.de>
2021-02-24 19:40:28 +0100

    msm8953-common: rro: Exclude CarrierConfig overlays from RRO
    They doesn't work right, because xml attributes where
    stripped down, and leading zeros are removed.
    
    E.g. instead of
     <carrier_config mcc="262" mnc="06">
    the values are modified and get
     <carrier_config mcc="262" mnc="6">
    
    This prevents matching of defined carrier definitions.
    
    Change-Id: I11efe716f9d2ea44bef80c15004d7a8cb71b58b9

--- msm8953.mk
+++ msm8953.mk
@@ -17,6 +17,9 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
